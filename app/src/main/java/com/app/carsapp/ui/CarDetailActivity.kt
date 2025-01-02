d1920d852152f75b68882cc05747a91a595ae3a6
vishnukumar.gr <vishnukumar.gr@changepond.com>
2025-01-02 13:18:45 +0530

    changes

--- app/src/main/java/com/app/carsapp/ui/CarDetailActivity.kt
+++ app/src/main/java/com/app/carsapp/ui/CarDetailActivity.kt
@@ -1,26 +1,29 @@
 package com.app.carsapp.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.app.carsapp.R
 import com.app.carsapp.databinding.ActivityCarDetailBinding
 import com.app.carsapp.model.CarResponse
+import com.app.carsapp.util.Constants.hide
+import com.app.carsapp.util.Constants.show
 import com.app.carsapp.util.Constants.toStrings
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CarDetailActivity : AppCompatActivity(), View.OnClickListener {
-
     companion object{
         const val CAR_ID="car_id"
     }
     private lateinit var binding: ActivityCarDetailBinding
     private val viewModel: MainViewModel by viewModels()
-    var edit : Boolean = false
-    lateinit var carResponse: CarResponse
+    private var edit : Boolean = false
+    private lateinit var carResponse: CarResponse
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,49 +43,81 @@ class CarDetailActivity : AppCompatActivity(), View.OnClickListener {
 
         binding.edit.setOnClickListener(this)
         binding.submit.setOnClickListener(this)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(enabled = true) {
+            override fun handleOnBackPressed() {
+                back()
+            }
+
+        })
     }
+
+    fun back(){
+        if(edit){
+            val alertBuild = AlertDialog.Builder(this)
+            alertBuild.setMessage("Discard Changes?")
+            alertBuild.setTitle("Alert!!")
+            alertBuild.setCancelable(false)
+            alertBuild.setPositiveButton("Yes"){
+                dialog,which->finish()
+            }
+            alertBuild.setNegativeButton("No"){
+                dialog,which -> dialog.cancel()
+            }
+            alertBuild.setNeutralButton("Save and exit"){ dialog,which ->
+                run {
+                    saveData()
+                    finish()
+                }
+            }
+            alertBuild.show()
+        }else finish()
+    }
+
+    fun saveData(){
+        carResponse.make=binding.carMake.text.toString().trim()
+        carResponse.model=binding.carModel.text.toString().trim()
+        carResponse.year=binding.carYear.text.toString().trim().toInt()
+        carResponse.color=binding.carColor.text.toString().trim()
+        carResponse.transmission=binding.carTransmission.text.trim().toString()
+        carResponse.engine=binding.carEngine.text.toString().trim()
+        carResponse.horsepower=binding.carHorsePower.text.toString().trim().toInt()
+        viewModel.setData(carResponse)
+    }
+
+    fun enabled(value : Boolean){
+        binding.carMake.isEnabled=value
+        binding.carYear.isEnabled=value
+        binding.carModel.isEnabled=value
+        binding.carColor.isEnabled=value
+        binding.carTransmission.isEnabled=value
+        binding.carEngine.isEnabled=value
+        binding.carHorsePower.isEnabled=value
+    }
+
 
     override fun onClick(view: View?) {
         when(view?.id){
             R.id.edit -> {
                 edit = true
                 binding.title.text=getString(R.string.car_edit_mode)
-                binding.edit.visibility= View.GONE
-                binding.submit.visibility=View.VISIBLE
-                binding.carMake.isEnabled=true
-                binding.carYear.isEnabled=true
-                binding.carModel.isEnabled=true
-                binding.carColor.isEnabled=true
-                binding.carTransmission.isEnabled=true
-                binding.carEngine.isEnabled=true
-                binding.carHorsePower.isEnabled=true
+                binding.edit.hide()
+                binding.submit.show()
+                enabled(true)
             }
             R.id.back->{
-                finish()
+                back()
             }
             R.id.submit -> {
                 edit =false
                 binding.title.text=getString(R.string.car_details)
-                binding.edit.visibility= View.VISIBLE
-                binding.submit.visibility=View.GONE
-                carResponse.make=binding.carMake.text.toString()
-                carResponse.model=binding.carModel.text.toString()
-                carResponse.year=binding.carYear.text.toString().toInt()
-                carResponse.color=binding.carColor.text.toString()
-                carResponse.transmission=binding.carTransmission.text.toString()
-                carResponse.engine=binding.carEngine.text.toString()
-                carResponse.horsepower=binding.carHorsePower.text.toString().toInt()
-                binding.carMake.isEnabled=false
-                binding.carYear.isEnabled=false
-                binding.carModel.isEnabled=false
-                binding.carColor.isEnabled=false
-                binding.carTransmission.isEnabled=false
-                binding.carEngine.isEnabled=false
-                binding.carHorsePower.isEnabled=false
-                viewModel.setData(carResponse)
+                binding.edit.show()
+                binding.submit.hide()
+                saveData()
+                enabled(false)
+
             }
         }
     }
-
 
 }
--- app/src/main/java/com/app/carsapp/util/Constants.kt
+++ app/src/main/java/com/app/carsapp/util/Constants.kt
@@ -1,13 +1,29 @@
 package com.app.carsapp.util
 
+import android.view.View
+
 object Constants {
     fun ArrayList<String?>?.toStrings() : String{
         if(this.isNullOrEmpty())
             return ""
         var str=""
-        this.forEach {
-            str += "$it, "
+        this.forEachIndexed { index, it ->
+            if(index==this.size-1){
+                str += "$it"
+            }else{
+                str += "$it\n"
+            }
         }
         return str.trim().substring(0,str.length-2)
     }
+
+    fun View.hide(){
+        this.visibility=View.GONE
+    }
+
+    fun View.show(){
+        this.visibility=View.VISIBLE
+    }
+
+
 }
